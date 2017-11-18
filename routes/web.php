3fdd80465414391c495359b0afdf15d475e80ce3
vagrant <vagrant@homestead>
2017-11-18 06:26:40 +0000

    Add column user_type in users table

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//use Auth;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+
+Route::get('/event', 'EventController@index');
+
+Route::get('/home', 'HomeController@index')->name('home');
+
